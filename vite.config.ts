74f7e63837febb9733cb5136fb9fc1c1bfae21bb
heightofjoytechnicalcollege <starlinkoxford@gmail.com>
2026-03-04 10:32:57 +0100

    Create vite.config.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,14 @@ import {defineConfig} from 'vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: { '@': path.resolve(__dirname, '.'), },
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
   },
   server: {
     port: 3000,
-    proxy: { '/api': 'http://localhost:3000' }
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
   }
 });
