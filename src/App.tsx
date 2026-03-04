8fcd59528480ae48c46604898030f78ade5a55aa
heightofjoytechnicalcollege <starlinkoxford@gmail.com>
2026-03-04 10:36:44 +0100

    Create App.tsx with main application component

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Height of Joy Technical College</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/student-portal">Student Portal</a></li>
+          <li><a href="/admission">Admission</a></li>
+          <li><a href="/teacher-login">Teacher Login</a></li>
+          <li><a href="/teacher-dashboard">Teacher Dashboard</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
